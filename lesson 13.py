2ccd003b2a9c822b07aec2688507e5adbba732a2
Grace-sims <gracesims2012@gmail.com>
2025-02-05 20:28:11 +0700

    Add files via upload

---
+++ lesson 13.py
@@ -0,0 +1,11 @@
+list1 = []
+fruits = ["apple", "grapes", "mango", "orange", "banana"]
+num = [1, 2, 3, 4, 5, 6]
+mixed = [1, 2, "donut", "false"]
+
+print ("fruits = ", fruits)
+
+print (len(mixed))
+
+print (mixed [0])
+print (mixed [-2])
